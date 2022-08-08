96d7277b28bbe396262d52123313e969ef72770d
FedeOrquin <fedeorquinn@gmail.com>
2022-08-08 19:42:35 -0300

    WIP

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b062b0ca5emsh620f758bef1c94dp10ca97jsn2013c06a8c3c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "904cef59a117be80c2bc5228b811ab9a";
